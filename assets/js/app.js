be3bebe6c6c1d063e9e42528df7fbc259108143a
DanyFadhilah <fadilahdany@gmail.com>
2019-09-29 00:41:21 +0700

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,48 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	$('#nav-open').click(function(){
+		$(this).toggleClass('open');
+		$('.menu-hide').toggleClass('menu-hide-active');
+	});
+});
+
+$(document).ready(function(){
+	$('.img').click(function(){
+		$(this).toggleClass('open');
+		$('.menu-hiden').toggleClass('menu-hiden-active');
+	});
+});
+
+$(document).ready(function(){
+	$('.vid').click(function(){
+		$('.popup').addClass('popup-active');
+		$('.popup').removeClass('popup-non');
+	});
+});
+
+$(document).ready(function(){
+	$('#balas').click(function(){
+		$(this).toggleClass('open');
+		$('.balasan').toggleClass('balasan-aktif');
+	});
+});
+
+$(document).ready(function(){
+	$('#balas1').click(function(){
+		$(this).toggleClass('open');
+		$('.balasan1').toggleClass('balasan1-aktif');
+	});
+});
+
+$(document).ready(function(){
+	$('#los').click(function(){
+		$('.popup').addClass('popup-non');
+		$('.popup').removeClass('popup-active');
+	});
+});
+
 $('.lovetoggle').click(function(){
 	$(this).toggleClass('actived');
 })
